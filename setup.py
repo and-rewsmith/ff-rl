e4c467a65c82bf0f800771658b524aa02230af22
Andrew Smith <andrew.smith.recruiting@gmail.com>
2025-02-21 12:14:08 -0800

    [ci]: init (#1)
    title

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='yours',
+    name='model',
     version='0.1.0',
     packages=find_packages(),
     install_requires=required,
